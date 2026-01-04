4c8df213c80a0061c66796e443ae187c7233b458
Clearleaf <jake.rubia@gmail.com>
2026-01-04 14:50:40 -0400

    added everything

---
+++ volume-slider/addons/BusVolumeSlider/BusVolumeSlider.gd
@@ -0,0 +1,16 @@
+class_name BusVolumeSlider extends HSlider
+
+@export var bus_name = "Master"
+
+var bus
+
+func _ready() -> void:
+	bus = AudioServer.get_bus_index(bus_name)
+	value = db_to_linear(AudioServer.get_bus_volume_db(bus))
+
+	min_value = 0.0
+	max_value = 1.0
+	step = 0.01
+	connect("value_changed",set_vol)
+
+func set_vol(val): AudioServer.set_bus_volume_db(bus, linear_to_db(val))
